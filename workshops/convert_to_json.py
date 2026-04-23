2a7314fa16b075da74c0434002d115f4838d07f4
supachai2547tar-hub <supachai2547tar@gmail.com>
2026-04-23 13:35:16 +0700

    physic_gate

--- workshops/convert_to_json.py
+++ workshops/convert_to_json.py
@@ -35,3 +35,17 @@ text_with_context = "ละเมิดลิขสิทธิ์ตามม�
 conf, sig = cal_confidence(text_with_context, 2)
 print(f"Confidence: {conf: 2f}")
 print(f"Signals: {sig}")
+
+#3. ลำดับศักดิ์ของข้อมูล เพื่อหาค่าน้ำหนักข้อมูล  เชื่อมต่อไปที่ 17
+def get_physic_gate_preview(predicted_class,text):
+    #0:None,1:Parent (hight complexity),2:copyright (medium complexity)
+    weight = {0:1,1:8.5,2:6.5}
+    base_weight = weight.get(predicted_class,1.0)
+    #ปรับน้ำหนักตามความรุนแรงที่อยู่ในข้อความ
+    if "ร้ายแรง" in text or "จำนวนมาก" in text:
+        base_weight = min(base_weight+1.0,10)
+    return base_weight
+# Run Program
+text = "การละเมิดสิทธิบัตรรายใหญ่"
+weight = get_physic_gate_preview(1,"text")
+print(f"Physics gate weight review : {weight}/10.0")
